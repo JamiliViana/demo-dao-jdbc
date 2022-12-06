60a9c00e7aedb8748e9b02d6e3509ab84ab47869
Jamili Viana <jamilimartucheliviana@gmail.com>
2022-12-05 23:52:10 -0300

    findByDepartment and findAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,7 +2,11 @@ package application;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Program {
     public static void main(String[] args) {
@@ -10,9 +14,21 @@ public class Program {
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
         System.out.println("=== TEST 1: seller findById ===");
-
         Seller seller = sellerDao.findById(3);
-
         System.out.println(seller);
+
+        System.out.println("\n=== TEST 2: seller findByDepartment ===");
+        Department department = new Department(2, null);
+        List<Seller> list = sellerDao.findByDepartment(department);
+        for (Seller obj : list) {
+            System.out.println(obj);
+        }
+
+        System.out.println("\n=== TEST 3: seller findAll =====");
+        list = sellerDao.findAll();
+        for (Seller obj : list) {
+            System.out.println(obj);
+        }
+
     }
 }
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -10,7 +10,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class SellerDaoJDBC implements SellerDao {
 
@@ -82,6 +85,69 @@ public class SellerDaoJDBC implements SellerDao {
 
     @Override
     public List<Seller> findAll() {
-        return null;
+        PreparedStatement st = null;
+        ResultSet rs = null;
+        try {
+            st = conn.prepareStatement(
+                    "SELECT seller.*,department.Name as DepName " +
+                            "FROM seller INNER JOIN department " +
+                            "ON seller.DepartmentId = department.Id " +
+                            "ORDER BY Name");
+            rs = st.executeQuery();
+            List<Seller>list = new ArrayList<>();
+            Map<Integer, Department> map = new HashMap<>();// instanciei um map vazio
+            while (rs.next()) {
+                Department dep = map.get(rs.getInt("DepartmentId"));//aqui ele vai ver no map se existe um departamento com o valor, se não existir vau retornar nulo
+
+                if (dep == null) {//aqui se não existir eu instancio o dep e salvo no map, para o proximo teste, eu pegar no map e não repitir
+                    dep = instantiateDepartment(rs);
+                    map.put(rs.getInt("DepartmentId"),dep);
+                }//se o departamento ja existir eu não vou instanciar ele duas vezes, eu passo por esse if e ja instancio o seller
+                Seller obj = instantiateSeller(rs, dep);
+                list.add(obj);
+            }
+            return list;
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatement(st);
+            DB.closeResultSet(rs);
+        }
+    }
+
+    @Override
+    public List<Seller> findByDepartment(Department department) {
+        PreparedStatement st = null;
+        ResultSet rs = null;
+        try {
+            st = conn.prepareStatement(
+                    "SELECT seller.*,department.Name as DepName " +
+                            "FROM seller INNER JOIN department " +
+                            "ON seller.DepartmentId = department.Id " +
+                            "WHERE DepartmentId = ? " +
+                            "ORDER BY Name");
+            st.setInt(1, department.getId());
+            rs = st.executeQuery();
+
+            List<Seller>list = new ArrayList<>();
+            Map<Integer, Department> map = new HashMap<>();// instanciei um map vazio
+            while (rs.next()) {
+
+                Department dep = map.get(rs.getInt("DepartmentId"));//aqui ele vai ver no map se existe um departamento com o valor, se não existir vau retornar nulo
+
+                if (dep == null) {//aqui se não existir eu instancio o dep e salvo no map, para o proximo teste, eu pegar no map e não repitir
+                    dep = instantiateDepartment(rs);
+                    map.put(rs.getInt("DepartmentId"),dep);
+                }//se o departamento ja existir eu não vou instanciar ele duas vezes, eu passo por esse if e ja instancio o seller
+                Seller obj = instantiateSeller(rs, dep);
+                list.add(obj);
+            }
+            return list;
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatement(st);
+            DB.closeResultSet(rs);
+        }
     }
 }
